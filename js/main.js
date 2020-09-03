292d959666a2b4f598ccfa4628a99007dfe98bb4
kenrelic <simeonkenneth3@gmail.com>
2020-09-03 12:33:31 +0100

    adds search bar animation and functionality

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const searchBar = document.getElementById('search-bar');
+const searchBtn = document.getElementById('search-icon');
+
+searchBtn.addEventListener('click', showSearchBar);
+searchBar.addEventListener('blur', hideSearchBar);
+
+function showSearchBar() {
+  if (searchBar.style.width === "" || searchBar.style.width === "0px") {
+    searchBar.style = `padding: 0.5em 0.5em 0.5em 1em; 
+    border: 1px solid #727272b3; 
+    width: 220px;`;
+    searchBar.focus();
+    searchBtn.removeEventListener('click', showSearchBar);
+    return searchBtn.addEventListener('click', searchLocation);
+  }
+}
+
+function searchLocation() {
+  if (searchBar.style.width !== "" && searchBar.style.width !== "0px") {
+    if (searchBar.value !== "") {
+      //make the API call
+      return console.log('...fetching data')
+    }
+  }
+}
+
+function hideSearchBar() {
+  if (searchBar.style.width !== "" || searchBar.style.width !== "0px") {
+    if (searchBar.value == "") {
+      searchBar.style = `padding: 0; 
+                        border: none; 
+                        width: 0px;`;
+      searchBtn.removeEventListener('click', searchLocation);
+      return searchBtn.addEventListener('click', showSearchBar);
+    }
+  }
+}
